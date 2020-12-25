2880c4c68f960f10037ea848cb121cf4db7fa245
kubota <yukiiz120110@gmail.com>
2020-12-25 19:39:21 +0900

    headerにGoogle Tag Managerを追加

--- wordpress/wp-content/themes/LINKS/header.php
+++ wordpress/wp-content/themes/LINKS/header.php
@@ -2,6 +2,13 @@
 <html lang="ja">
 
 <head>
+    <!-- Google Tag Manager -->
+<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-MSWRLMQ');</script>
+<!-- End Google Tag Manager -->
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <title>
         <?php bloginfo('name'); ?>
@@ -22,6 +29,10 @@
 </head>
 
 <body <?php body_class(); ?>>
+<!-- Google Tag Manager (noscript) -->
+<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-MSWRLMQ"
+height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+<!-- End Google Tag Manager (noscript) -->
     <div class="content-wrap">
         <!-- header -->
         <header class="header">
